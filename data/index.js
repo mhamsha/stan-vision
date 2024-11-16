e07f0b26d2daa13bd48630c02819342eda5f2ed7
mhamsha <hamza.shahzad.8692@gmail.com>
2024-11-16 23:35:32 +0500

    Add SVG assets and initial JavaScript file for website features

---
+++ data/index.js
@@ -0,0 +1,54 @@
+const cards = [
+  {
+    id: 1,
+    title: "Website design",
+    description:
+      "Give visitors a reason for a higher price with a website telling the visual story of your ideal customers and why they should choose your product over the competition right now.",
+    img: "/public/assets/svg/card-1.svg",
+    alt: "Website design",
+    imgHover: "/public/assets/svg/card-1.1.svg",
+  },
+  {
+    id: 2,
+    title: "Digital strategy",
+    description:
+      "From UX audits, competitive research, and customer journey maps to user experience flows, visual language, and website redesign - StanVision helps you build the entire product and brand design strategy for your SaaS company.",
+    img: "/public/assets/svg/card-2.svg",
+    alt: "Digital strategy",
+    imgHover: "/public/assets/svg/card-2.1.svg",
+  },
+  {
+    id: 3,
+    title: "Webflow development",
+    description:
+      "Give your product a modern experience with a Webflow website built by designers and developers who live and breathe SaaS design and know how to talk to your product team.",
+    img: "/public/assets/svg/card-3.svg",
+    alt: "Mobile app design",
+    imgHover: "/public/assets/svg/card-3.1.svg",
+  },
+  {
+    id: 4,
+    title: "Product design",
+    description:
+      "Get a product design that helps you stand out in the market and attract the right customers. StanVision helps you build a product that your customers will love and that will help you grow your business.",
+    img: "/public/assets/svg/card-4.svg",
+    alt: "Product design",
+    imgHover: "/public/assets/svg/card-4.1.svg",
+  },
+  {
+    id: 5,
+    title: "Support+growth",
+    description:
+      "Startups and SaaS teams have enough growth problems. Leave the project management, A/B testing, and optimization to StanVision - your long-term partner and design co-founder.",
+    img: "/public/assets/svg/card-5.svg",
+    alt: "Support+growth",
+    imgHover: "/public/assets/svg/card-5.1.svg",
+  },
+];
+
+const cardsSvgs = {
+  cardArrow: "/public/assets/svg/card-arrow.svg",
+  cardCross: "/public/assets/svg/card-cross.svg",
+  cardLine: "/public/assets/svg/card-line.svg",
+  cardPlus: "/public/assets/svg/card-plus.svg",
+};
